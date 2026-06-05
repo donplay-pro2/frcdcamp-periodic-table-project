0c7957d7b76b9ffa8868004c5c0351517703e161
Abdul Alim <72149305+donplay-pro2@users.noreply.github.com>
2026-06-05 18:55:43 +0530

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,20 +1,23 @@
 #! /bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-if [[ $1 ]]
+INPUT_ARG=$1
+if [[ $INPUT_ARG ]]
 then
-  if [[ $1 =~ ^[0-9]+$ ]]
+  #check input
+  if [[ $INPUT_ARG =~ ^[0-9]+$ ]]
   then
     # If it is a number, query by atomic_number only
-    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$INPUT_ARG")
   else
     # If it is not a number, query by symbol or name
-    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1' OR name='$1'")
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$INPUT_ARG' OR name='$INPUT_ARG'")
   fi
   
+  #check if ATOM not exsist
   if [[ -z $ATOMIC_NUMBER ]]
   then
-    echo -e "I could not find that element in the database."
+    echo "I could not find that element in the database."
   else
     while IFS=$'|' read -r SYMBOL NAME MASS MELT BOIL TYPE; 
     do
@@ -22,5 +25,5 @@ then
     done < <($PSQL "SELECT symbol,name,atomic_mass,melting_point_celsius,boiling_point_celsius,type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$ATOMIC_NUMBER")
   fi
 else
-  echo -e "Please provide an element as an argument."
+  echo "Please provide an element as an argument."
 fi
